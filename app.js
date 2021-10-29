32c19083ecd9b35ff6edc7dd3429fb4840cf2bfe
mr-chidex <mbahchiboy2@gmail.com>
2021-10-29 14:15:28 +0100

    promisifying redis client

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const redis = require("redis");
+const responseTime = require("response-time");
+const { promisify } = require("util");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 const REDIS_PORT = process.env.PORT || 6379;
-
+app.use(responseTime());
 const client = redis.createClient(REDIS_PORT);
+const GET_CLIENT = promisify(client.get).bind(client);
+const SET_CLIENT = promisify(client.setex).bind(client);
 
 const getResponse = async (req, res) => {
   try {
@@ -15,26 +19,25 @@ const getResponse = async (req, res) => {
     const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
 
+    if (!data)
+      return res.status(400).json({ message: "invalid username entered" });
+
     //set to redis
-    client.setex(username, 3600, JSON.stringify(data?.public_repos));
+    await SET_CLIENT(username, 3600, JSON.stringify(data?.public_repos));
 
     res.json(data.public_repos);
   } catch (err) {
-    console.log(err.message);
     res.status(500).json({ message: err.message });
   }
 };
 
-const cacheMiddleware = (req, res, next) => {
+const cacheMiddleware = async (req, res, next) => {
   const { username } = req.params;
 
-  client.get(username, (error, data) => {
-    if (error) return next(error);
+  const data = await GET_CLIENT(username);
 
-    if (data !== null) return res.json(JSON.parse(data));
-
-    next();
-  });
+  if (data) return res.json(JSON.parse(data));
+  next();
 };
 
 app.get("/:username", cacheMiddleware, getResponse);
